f5fc27a80dad324b3964b0d716164f7231968764
rudra-9197 <91685979+rudra-9197@users.noreply.github.com>
2021-12-07 21:52:59 +0530

    logic for triangle

---
+++ triangle.js
@@ -0,0 +1,21 @@
+const firstAngle = document.querySelector("#first-angle");
+const secondAngle = document.querySelector("#second-angle");
+const thirdAngle = document.querySelector("#third-angle");
+var checkButton = document.querySelector("#check");
+var ouput = document.querySelector("#output");
+
+function checkSumOfAngle() {
+  console.log(thirdAngle.value);
+  sum =
+    Number(firstAngle.value) +
+    Number(secondAngle.value) +
+    Number(thirdAngle.value);
+
+  if (sum == 180) {
+    ouput.innerText = "The angles will form a Triangle";
+  } else {
+    ouput.innerText = "ooops!...The Angles will not form a Triangle";
+  }
+}
+
+checkButton.addEventListener("click", checkSumOfAngle);
