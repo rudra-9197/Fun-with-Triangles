b0dbfabb35503edaf3e92408a14dd31e5f793d2d
rudra-9197 <91685979+rudra-9197@users.noreply.github.com>
2021-12-08 13:43:15 +0530

    quiz

---
+++ quiz.js
@@ -0,0 +1,21 @@
+const quizForm = document.querySelector(".quiz-form");
+const SubmitAnsButton = document.querySelector("#submit-ans-button");
+const output = document.querySelector("#output");
+
+
+const correctAns = ["90°","250 sq.m"]
+
+function calculateScore(){
+    let score =0;
+    let index = 0;
+    const formResults = new FormData(quizForm);
+    for(let value of formResults.values()){
+        if(value === correctAns[index]){
+            score = score+1;
+
+        }
+     index =index + 1;
+    }
+output.innerText ="your score is "+ score;
+}
+SubmitAnsButton.addEventListener("click",calculateScore);
