5728bb654fd5b139b016b1f1d91f7133b48d4899
rudra-9197 <91685979+rudra-9197@users.noreply.github.com>
2021-12-08 17:51:47 +0530

    area of triangle

---
+++ area.js
@@ -0,0 +1,29 @@
+const firstSide = document.querySelector("#first-side");
+const secondSide = document.querySelector("#second-side");
+const thirdSide = document.querySelector("#third-side");
+const checkButton = document.querySelector("#check-Button");
+const output =document.querySelector("#output");
+
+
+
+
+function calculateArea(){
+a=Number(firstSide.value)
+b = Number(secondSide.value)
+c= Number(thirdSide.value)
+
+    sp = ((a+b+c)/2);
+area =Math.sqrt( sp*(sp-a)*(sp-b)*(sp-c))
+if (area==0 || NaN ){
+    ouput.innerText = "please enter a valid sides";}
+else{
+output.innerText = "The area of Triangle is "+ area + " m."
+    }
+
+
+    
+}
+
+
+
+checkButton.addEventListener("click",calculateArea)
